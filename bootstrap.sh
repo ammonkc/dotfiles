eddb7c67f261488217df2ea4d7995106b9a7bc7c
Ammon Casey <ammon@caseyohana.com>
2013-11-11 17:28:02 -1000

    [bootstrap] added vim-airline config to after.vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -96,24 +96,62 @@ vimify() {
     fi
 
     if [ ! -f after.vimrc ]; then
-      echo '" Colorscheme' >> 'after.vimrc'
-      echo 'syntax enable' >> 'after.vimrc'
-      echo 'set background=dark' >> 'after.vimrc'
-      echo 'colorscheme solarized' >> 'after.vimrc'
+      cat << EOF > 'after.vimrc'
+        " Colorscheme
+        syntax enable
+        set background=dark
+        colorscheme solarized
 
-      echo '" Map arrow keys' >> 'after.vimrc'
-      echo 'nmap <Left> <Left>' >> 'after.vimrc'
-      echo 'nmap <Right> <Right>' >> 'after.vimrc'
-      echo 'nmap <Up> <Up>' >> 'after.vimrc'
-      echo 'nmap <Down> <Down>' >> 'after.vimrc'
+        if !exists('g:airline_symbols')
+          let g:airline_symbols = {}
+        endif
 
-      echo 'if &term == "xterm-ipad"' >> 'after.vimrc'
-      echo 'nnoremap <Tab> <Esc>' >> 'after.vimrc'
-      echo 'vnoremap <Tab> <Esc>gV' >> 'after.vimrc'
-      echo 'onoremap <Tab> <Esc>' >> 'after.vimrc'
-      echo 'inoremap <Tab> <Esc>`^' >> 'after.vimrc'
-      echo 'inoremap <Leader><Tab> <Tab>' >> 'after.vimrc'
-      echo 'endif' >> 'after.vimrc'
+        " unicode symbols
+        "let g:airline_left_sep = '»'
+        "let g:airline_left_sep = '▶'
+        "let g:airline_right_sep = '«'
+        "let g:airline_right_sep = '◀'
+        "let g:airline_symbols.linenr = '␊'
+        "let g:airline_symbols.linenr = '␤'
+        "let g:airline_symbols.linenr = '¶'
+        "let g:airline_symbols.branch = '⎇'
+        "let g:airline_symbols.paste = 'ρ'
+        "let g:airline_symbols.paste = 'Þ'
+        "let g:airline_symbols.paste = '∥'
+        "let g:airline_symbols.whitespace = 'Ξ'
+
+        " powerline symbols
+        "let g:airline_left_sep = ''
+        "let g:airline_left_alt_sep = ''
+        "let g:airline_right_sep = ''
+        "let g:airline_right_alt_sep = ''
+        "let g:airline_symbols.branch = ''
+        "let g:airline_symbols.readonly = ''
+        "let g:airline_symbols.linenr = ''
+
+        " old vim-powerline symbols
+        let g:airline_left_sep = '⮀'
+        let g:airline_left_alt_sep = '⮁'
+        let g:airline_right_sep = '⮂'
+        let g:airline_right_alt_sep = '⮃'
+        let g:airline_symbols.branch = '⭠'
+        let g:airline_symbols.readonly = '⭤'
+        let g:airline_symbols.linenr = '⭡'
+
+        " Map arrow keys
+        nmap <Left> <Left>
+        nmap <Right> <Right>
+        nmap <Up> <Up>
+        nmap <Down> <Down>
+
+        if &term == "xterm-ipad"
+          nnoremap <Tab> <Esc>
+          vnoremap <Tab> <Esc>gV
+          onoremap <Tab> <Esc>
+          inoremap <Tab> <Esc>\`^
+          inoremap <Leader><Tab> <Tab>
+        endif
+      EOF
     fi
 
     cd "$INSTALLDIR"
