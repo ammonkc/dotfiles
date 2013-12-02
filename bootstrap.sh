6c9ca2f22c6972eb2a246496cd8823af81fbc2f5
Ammon Casey <ammon@caseyohana.com>
2013-12-02 13:43:38 -1000

    [bootstrap] fixed https github url

--- bootstrap.sh
+++ bootstrap.sh
@@ -240,7 +240,7 @@ if [ -d $HOME/.dotfiles ]; then
 else
   # Clone Repo
   notice "Downloading"
-  git clone --recursive https://github.com:ammonkc/dotfiles.git $HOME/.dotfiles
+  git clone --recursive https://github.com/ammonkc/dotfiles.git $HOME/.dotfiles
 
   pushd $HOME/.dotfiles
 
