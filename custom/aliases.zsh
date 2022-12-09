7460a038c6c50075305b61d0e8f59be1de1b20cc
Ammon Casey <ammon@caseyohana.com>
2022-12-09 11:41:10 -1000

    [alias] added sites and src aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -72,9 +72,8 @@ alias fs="stat -f \"%z bytes\""
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
-alias docs="lara && cd docs/"
+alias sites="cd $HOME/Developer/code"
+alias src="cd $HOME/Developer/src"
 
 # Laravel
 alias a="php artisan"
