4353ccfd24c9d697829c8d079b10f40d732fc85c
Ammon Casey <ammon@caseyohana.com>
2024-04-19 11:46:06 -1000

    remove fzf-git from exports.sh

--- bin/dotfiles-sync.sh
+++ bin/dotfiles-sync.sh
@@ -3,10 +3,14 @@
 # Sync dotfiles repo and ensure that dotfiles are tangled correctly afterward
 notice "Syncing dotfiles repo"
 
-# Navigate to the directory of the dotfiles(generally ~/.dotfiles)
-cd $DOTFILES
+# Navigate to the directory of this script (generally ~/.dotfiles/.bin)
+notice "dir: $(dirname $(readlink -f $0))"
+# cd $(dirname $(readlink -f $0))
+# cd ..
+pushd $DOTFILES
 
 notice "Stashing existing changes..."
+echo "current dir: $(pwd)"
 stash_result=$(git stash push -m "sync-dotfiles: Before syncing dotfiles")
 needs_pop=1
 if [ "$stash_result" = "No local changes to save" ]; then
--- install/exports.sh
+++ install/exports.sh
@@ -111,8 +111,6 @@ _fzf_compgen_dir() {
     fd --type=d --hidden --exclude .git . "$1"
 }
 
-source ~/fzf-git.sh/fzf-git.sh
-
 export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
 export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 
