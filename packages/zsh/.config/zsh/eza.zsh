ae0dd4c5714301a2a795cb4073220da70066a5c8
Ammon Casey <ammon@caseyohana.com>
2025-12-03 11:30:36 -1000

    fix: update eza theme configuration to use tokyonight as the default theme for improved aesthetics

--- packages/zsh/.config/zsh/eza.zsh
+++ packages/zsh/.config/zsh/eza.zsh
@@ -2,7 +2,7 @@
 # ---- eza -----
 # -----------------------
 
-EZA_THEME="${EZA_THEME:-catppuccin}"
+EZA_THEME="${EZA_THEME:-tokyonight}"
 EZA_COLOR_SCHEME="${EZA_COLOR_SCHEME:-dark}"
 local eza_theme_path="$XDG_CONFIG_HOME/eza/themes/$EZA_THEME.yml"
 
