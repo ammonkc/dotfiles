e4b7786ba18c40d27864d52d1cbf3cd8a7ac33d1
Ammon Casey <ammon@caseyohana.com>
2025-12-08 12:07:48 -1000

    chore: add script to switch PHP versions in Raycast for easier development

---
+++ packages/raycast/.config/raycast/script-commands/switch-php.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch PHP Version
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon https://www.php.net/favicon.svg
+# @raycast.packageName php version
+# @raycast.argument1 { "type": "dropdown", "placeholder": "version", "optional": true, "data": [{"title": "8.0", "value": "8.0"}, {"title": "8.1", "value": "8.1"}, {"title": "8.2", "value": "8.2"}, {"title": "8.3", "value": "8.3"}, {"title": "8.4", "value": "8.4"}] }
+
+# Documentation:
+# @raycast.description Switch PHP version (leave empty to show current version)
+# @raycast.author Ammon Casey
+
+# Use zsh to call the phpswitch function from dotfiles
+/bin/zsh -ic "phpswitch $1"
