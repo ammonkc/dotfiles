d3bbd217464af6190466a6bf886a3c86b3040fb3
Ammon Casey <ammon@caseyohana.com>
2026-01-09 08:52:10 -1000

    feat: add PHP 8.5 option to switch-php.sh dropdown for version selection

--- packages/raycast/.config/raycast/script-commands/switch-php.sh
+++ packages/raycast/.config/raycast/script-commands/switch-php.sh
@@ -8,7 +8,7 @@
 # Optional parameters:
 # @raycast.icon ~/.config/raycast/script-commands/icons/php.svg
 # @raycast.packageName php version
-# @raycast.argument1 { "type": "dropdown", "placeholder": "version", "optional": true, "data": [{"title": "8.0", "value": "8.0"}, {"title": "8.1", "value": "8.1"}, {"title": "8.2", "value": "8.2"}, {"title": "8.3", "value": "8.3"}, {"title": "8.4", "value": "8.4"}] }
+# @raycast.argument1 { "type": "dropdown", "placeholder": "version", "optional": true, "data": [{"title": "8.0", "value": "8.0"}, {"title": "8.1", "value": "8.1"}, {"title": "8.2", "value": "8.2"}, {"title": "8.3", "value": "8.3"}, {"title": "8.4", "value": "8.4"}, {"title": "8.5", "value": "8.5"}] }
 
 # Documentation:
 # @raycast.description Switch PHP version (leave empty to show current version)
