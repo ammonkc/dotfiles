081e5657387588c5736c3ac47cf20bbf50424561
Ammon Casey <ammon@caseyohana.com>
2026-04-29 11:46:26 -1000

    refactor: remove deprecated refactoring plugin and add new configuration for ThePrimeagen/refactoring.nvim

---
+++ packages/nvim/.config/nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,98 @@
+-- Stand-in for lazyvim.plugins.extras.editor.refactoring: older LazyVim loads
+-- telescope.load_extension("refactoring"), but ThePrimeagen/refactoring.nvim
+-- no longer provides that extension, which errors when Telescope lazy-loads.
+
+if vim.fn.has("nvim-0.12") == 0 then
+  if _G.LazyVim and type(LazyVim) == "table" and type(LazyVim.error) == "function" then
+    LazyVim.error("refactoring.nvim requires Neovim 0.12 or higher", { title = "refactoring.nvim" })
+  end
+  return {}
+end
+
+return {
+  { "lewis6991/async.nvim", lazy = true },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    keys = {
+      { "<leader>r", "", desc = "+refactor", mode = { "n", "x" } },
+      {
+        "<leader>rs",
+        function()
+          return require("refactoring").select_refactor()
+        end,
+        mode = { "n", "x" },
+        desc = "Select Refactor",
+      },
+      {
+        "<leader>ri",
+        function()
+          return require("refactoring").inline_var()
+        end,
+        mode = { "n", "x" },
+        desc = "Inline Variable",
+        expr = true,
+      },
+      {
+        "<leader>rP",
+        function()
+          return require("refactoring.debug").print_loc({ output_location = "below" })
+        end,
+        desc = "Debug Print Location",
+        expr = true,
+      },
+      {
+        "<leader>rp",
+        function()
+          return require("refactoring.debug").print_var({ output_location = "below" }) .. "iw"
+        end,
+        mode = { "n", "x" },
+        desc = "Debug Print Variable",
+        expr = true,
+      },
+      {
+        "<leader>rc",
+        function()
+          return require("refactoring.debug").cleanup({ restore_view = true }) .. "ag"
+        end,
+        desc = "Debug Cleanup",
+        expr = true,
+      },
+      {
+        "<leader>rf",
+        function()
+          return require("refactoring").extract_func()
+        end,
+        mode = { "n", "x" },
+        desc = "Extract Function",
+        expr = true,
+      },
+      {
+        "<leader>rF",
+        function()
+          return require("refactoring").extract_func_to_file()
+        end,
+        mode = { "n", "x" },
+        desc = "Extract Function To File",
+        expr = true,
+      },
+      {
+        "<leader>rx",
+        function()
+          return require("refactoring").extract_var()
+        end,
+        mode = { "n", "x" },
+        desc = "Extract Variable",
+        expr = true,
+      },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("refactoring").setup(opts)
+    end,
+  },
+}
